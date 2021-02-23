d363628ef8690572822abcec166faada43a16d8f
sumiya-mb <sumiya@mango-byte.com>
2021-02-23 14:56:14 +0900

    create pull request

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers</strong>{{ followers }}
+    <button @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  // State
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_AaaaaBbbbb',
+        firstName: 'Aaaaa',
+        lastName: 'Bbbbb',
+        email: 'aaaaabbbbb@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  // Computed values are very valuable for manipulating data that exists on your Vue. Whenever you want to filter or transform your data, typically you will use a computed value for that purpose.
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  // A method is just a function bound to the Vue instance. It will only be evaluated when you explicitly call it. You use methods to react to events which happen in the DOM, or you can call them from elsewhere within your component
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  // Run once when the component is mounted
+  mounted() {
+    console.log('Page mounted')
+    this.followUser();
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
