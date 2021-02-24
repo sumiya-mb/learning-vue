b4abe7324c13e6ae57e97d16199fe72767c83099
sumiya-mb <sumiya@mango-byte.com>
2021-02-24 21:36:07 +0900

    Added v-if to display div conditionally

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,14 @@
 <template>
-  <div id="app">
-    @{{ user.username }} - {{ fullName }}
-    <strong>Followers</strong>{{ followers }}
-    <button @click="followUser">Follow</button>
-  </div>
+  <UserProfile/>
 </template>
 
 <script>
 
+import UserProfile from "@/components/UserProfile";
+
 export default {
   name: 'App',
-  // State
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: '_AaaaaBbbbb',
-        firstName: 'Aaaaa',
-        lastName: 'Bbbbb',
-        email: 'aaaaabbbbb@gmail.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch: {
-    followers(newValue, oldValue) {
-      if (oldValue < newValue) {
-        console.log(`${this.user.username} has gained a follower!`)
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    }
-  },
-  mounted() {
-    console.log('Page mounted')
-    this.followUser();
-  }
+  components: {UserProfile}
 }
 </script>
 
@@ -54,8 +18,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong>{{ followers }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_AaaaaBbbbb',
+        firstName: 'Aaaaa',
+        lastName: 'Bbbbb',
+        email: 'aaaaabbbbb@gmail.com',
+        isAdmin: true,
+      }
+    }
+  },
+  watch: {
+    followers(newValue, oldValue) {
+      if (oldValue < newValue) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    console.log('Page mounted')
+    this.followUser();
+  }
+}
+</script>
+
+<style scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E8;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
