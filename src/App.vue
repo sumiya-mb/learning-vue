4d28b3f6af38772098da802d15e00063945f9377
sumiya-mb <sumiya@mango-byte.com>
2021-03-07 16:26:23 +0900

    Temp commit

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">
-      <div class="navigation__logo">
-        Twitter clone
-      </div>
-    </router-link>
-    <div class="navigation__user" v-if="user">
-      {{ user.username }}
-    </div>
-  </nav>
-  <UserProfile/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
 
-import UserProfile from "@/components/UserProfile";
-
-export default {
-  name: 'App',
-  components: {UserProfile},
-  data(){
-    return{
-      user:{
-        username: '_ThisIsFake'
-      }
-    }
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+}
 
-  nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 5%;
-    background-color: deeppink;
-    color: white;
-    .navigation__logo {
-      font-weight: bold;
-      font-size: 24px;
-    }
-    .navigation__user {
-      font-weight: bold;
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
